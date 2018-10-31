11fe318b46c037b1a9823fcdd5e30b0baf02c621
ed42311 <ed42311@gmail.com>
2018-10-30 22:11:49 -0600

    one pane working, and play function

--- app.js
+++ app.js
@@ -7,30 +7,189 @@ const index = require("./routes/index");
 const app = express();
 app.use(index);
 const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server); 
 
-const getApiAndEmit = async socket => {
+class DataStore {
+  constructor(io){
+    this.io = io
+    this.games = []
+    this.num = 0
+  }
+
+  addNewGame(game) {
+    this.games.push(game)
+  }
+
+  removeEmptyGames() {
+    this.games = this.games.filter(game => !(game.player1 === null && game.player2 === null))
+  }
+
+  getGameByPlayerId(id) {
+    const { games } = this
+    for (let i = 0; i < games.length; i++) {
+      let counter = 1
+      while(counter <= 2) {
+        let player = `player${counter}`
+        if(games[i][player].getSocketId() === id) {
+          return games[i]
+        } 
+        counter++
+      }
+    }
+  }
+
+  removePlayerWith(id) {
+    const { games } = this
+    for (let i = 0; i < games.length; i++) {
+      let counter = 1
+      while(counter <= 2) {
+        let player = `player${counter}`
+        if(games[i][player].getSocketId() === id) {
+          games[i][player] = null
+          games[i].updateStatus()
+          break
+        } 
+        counter++
+      }
+    }
+  }
+
+  getGameNum() {
+    return this.num
+  }
+
+  getGameName() {
+    return `game${this.num}`
+  }
+
+  setNumberPlus(){
+    this.num++
+  }
+}
+
+const gameStore = new DataStore(io)
+
+class Player {
+  constructor(socket){
+    this.id = socket.id
+    this.socket = socket
+  }
+
+  getSocketId() {
+    return this.socket.id
+  }
+}
+
+class Game {
+  constructor(player1 = null, player2 = null, name){
+    this.name = name
+    this.player1 = player1
+    this.player2 = player2
+    this.status = (player1 && player2) ? "Playing" : "Waiting..."
+  }
+
+  getStatus() {
+    return this.status
+  }
+
+  getName() {
+    return this.name
+  }
+
+  isPlaying() {
+    return !(this.player1 && this.player2)
+  }
+
+  isWaiting() {
+    if(this.player1 && !this.player2) {
+      return true
+    } 
+  }
+
+  updateStatus() {
+    const { player1, player2 } = this
+    this.status = (player1 && player2) ? "Playing" : "Waiting..."
+  }
+
+  addPlayerTwo(player) {
+    this.player2 = player 
+    this.status = "Playing"
+  }
+}
+
+const createNewGame = async (socket, store) => {
+  await socket.join(store.getGameName())
+  const newPlayer = new Player(socket)
+  const currentGame = new Game(newPlayer, null, store.getGameName())
+
+  store.games.push(currentGame)
+}
+
+const addPlayerTwoToGame =  async(socket, current, store) => {
+  await socket.join(current.getName())
+  const newPlayer = new Player(socket)
+  current.addPlayerTwo(newPlayer)
+  console.log(store.games)
+}
+
+const findLastWaiting = (games) => {
+  const waiting = games.filter(game => game.isWaiting())
+  if(waiting.length === 0) {
+    return false
+  } else {
+    return waiting.pop()
+  }
+}
+
+const join = async socket => {
   try {
-    const res = await axios.get(
-      "https://api.darksky.net/forecast/2b7e84c6a76b88af55bd3fac37cbbb52/37.8267,-122.4233"
-    ); // Getting the data from DarkSky
-    socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+    const { games } = gameStore
+    const isEmpty = !(io.sockets.adapter.rooms[gameStore.getGameName()])
+  
+    if(isEmpty) {
+      await createNewGame(socket, gameStore)
+    } else {
+      if(io.sockets.adapter.rooms[gameStore.getGameName()].length === 1) {
+        const waiting = findLastWaiting(games);
+        if(waiting){
+          await addPlayerTwoToGame(socket, waiting, gameStore)
+        }
+      } else {
+        console.log("game room is full")
+      }
+    } 
+    await socket.emit('join', 'test')
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
 };
 
-let interval;
+const message = async data => {
+  console.log(data)
+}
+
+const playPresses = async (data, socket) => {
+  await socket.emit("otherPresses", data)
+}
+
+const disconnect = async (socket, store) => {
+  try {
+    await store.removePlayerWith(socket.id)
+    await store.removeEmptyGames()
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
 
 io.on("connection", socket => {
+  let store = new DataStore(io)
   console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 10000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
+
+  join(socket);
+
+  socket.on("message", message); 
+  socket.on("presses", (data) => playPresses(data, socket)); 
+  socket.on("disconnect", () => disconnect(socket, gameStore));
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- socket-io-client/src/App.js
+++ socket-io-client/src/App.js
@@ -1,27 +1,77 @@
 import React, { Component } from "react";
-import socketIOClient from "socket.io-client";
+import { socket, join, sendMessage, playPresses, sendPresses } from "./subscribe"
 class App extends Component {
   constructor() {
     super();
     this.state = {
       response: false,
-      endpoint: "http://127.0.0.1:4001"
+      greeting: '',
+      timestamp: '',
+      letters: ['h','e','l','l','o'],
+      presses: [],
+      playerPresses: []
     };
+    // set state in constructor
+    // playPresses((err, presses) => this.setState({presses}))
   }
+
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("FromAPI", data => this.setState({ response: data }));
+    // join event occuring on compDidMount
+    join((err, response) => this.setState({response}))
+    // playPresses((err, presses) => this.setState({presses}))
   }
+
+  pressButtons(presses) {
+    const pressesMock = (e, i) => {
+      return setTimeout(function() {
+        document.getElementById(`btn-${e}`).style.backgroundColor = 'red'
+      }, (500 * i));
+    }
+    presses.forEach((e, i) =>  pressesMock(e, i));
+  }
+
+  onClick = () => {
+    console.log(this.state.presses)
+  }
+
+  letterClick = (i) => {
+    const { playerPresses } = this.state
+    playerPresses.push(i)
+    this.setState({playerPresses})
+  }
+
+  sendPresses = async () => {
+    const { playerPresses } = this.state
+    await sendPresses(playerPresses)
+    await playPresses((err, presses) => this.pressButtons(presses))
+  }
+
   render() {
     const { response } = this.state;
     return (
-      <div style={{ textAlign: "center" }}>
-        {response
-          ? <p>
-              The temperature in Florence is: {response} °F
-            </p>
-          : <p>Loading...</p>}
+      <div>
+        <div style={{ textAlign: "center" }}>
+          {response
+            ? <p>
+                {response}
+              </p>
+            : <p>Loading...</p>}
+        </div>
+        <p>{this.state.greeting}</p>
+        <button onClick={this.onClick} >Start Play</button>
+        <button onClick={this.sendPresses}>Send Presses</button>
+        <div style={{ display : 'flex', justifyContent: 'center'}}>
+          {this.state.letters.map( (l, i) => 
+            <button 
+              key={`btn-${i}`}
+              id={`btn-${i}`}
+              onClick={() => this.letterClick(i)}
+              style={{width: '30%', height: '85px'}} 
+            > 
+              {l.toUpperCase()} 
+            </button> 
+          )}
+        </div>  
       </div>
     );
   }
